f232066f76e267d1eaad5fb5f3f7f834d391fd2e
Satya <satyab4u.777@gmail.com>
2018-11-12 20:07:16 -0500

    Added Coins Change problem with memoziation

--- src/com/satya/CoinsChange.java
+++ src/com/satya/CoinsChange.java
@@ -1,45 +1,47 @@
 package com.satya;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by Satya on 10/28/18.
  */
 public class CoinsChange {
 
     public static void main(String args[]){
-
         System.out.println("Main method started....");
         int coins[]={1,2,3};
-
         int target= 4;
         System.out.println(makeChange(coins,target));
-
-
-
     }
 
     public static long makeChange(int[] coins, int money){
-        return makeChange(coins,money,0);
-
+        Map<String,Long> coinsMap=new HashMap<>();
+        long ways= makeChange(coins,money,0, (HashMap<String, Long>) coinsMap);
+        for (Map.Entry<String, Long> mapEntry:coinsMap.entrySet()) {
+            System.out.println("key:"+mapEntry.getKey()+" value: "+mapEntry.getValue());
+        }
+        return ways;
     }
 
-    public static long makeChange(int[] coins,int money,int index){
-
+    public static long makeChange(int[] coins, int money, int index, HashMap<String,Long> memo){
         if(money == 0)
-        {
             return 1;
-        }
-        if(index >= coins.length){
+        if(index >= coins.length)
             return 0;
+        int amountOfCoin=0;
+        String key= money+"-"+index;
+        if(memo.containsKey(key)){
+            return memo.get(key);
         }
-        int amountWithCoins=0;
         long ways=0;
-
-        while (amountWithCoins <= money){
-            int remaining = money-amountWithCoins;
-            ways += makeChange(coins,remaining,index+1);
-            amountWithCoins += coins[index];
-
+        while(amountOfCoin <= money) {
+            int remaining=money-amountOfCoin;
+            ways+=makeChange(coins,remaining,index + 1,memo);
+            amountOfCoin+= coins[index];
         }
+        memo.put(key,ways);
         return ways;
     }
 }
